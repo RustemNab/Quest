4c3d798e869996d68f537cbdd26ce3000604826a
RustemNab <runabi29@gmail.com>
2017-04-25 18:42:21 +0300

    activities changed

--- app/src/main/java/com/questqazan/androidprojectv2/GameInterfaceActivity.java
+++ app/src/main/java/com/questqazan/androidprojectv2/GameInterfaceActivity.java
@@ -1,7 +1,10 @@
 package com.questqazan.androidprojectv2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Space;
+import android.view.View;
 
 public class GameInterfaceActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class GameInterfaceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_interface);
+
+        final Space questions = (Space) findViewById(R.id.go_questions);
+        final Space tasks = (Space) findViewById(R.id.go_tasks);
+
+
+        questions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(GameInterfaceActivity.this, QuestionsActivity.class);
+                GameInterfaceActivity.this.startActivity(intent1);
+            }
+        });
+
+        tasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(GameInterfaceActivity.this, TasksActivity.class);
+                GameInterfaceActivity.this.startActivity(intent1);
+            }
+        });
     }
 }
+
+
